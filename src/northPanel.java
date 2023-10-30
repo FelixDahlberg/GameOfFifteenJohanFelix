b16d4beffc39dfdf6cf82859fe7c792aea584373
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2023-10-30 11:51:45 +0100

    Småjusteringar

--- src/northPanel.java
+++ src/northPanel.java
@@ -4,19 +4,19 @@ import java.awt.event.ActionListener;
 
 public class northPanel extends JPanel implements ActionListener {
     
-    JButton newGameButton = new JButton("New Game");
+    JButton newGameButton = new JButton("New Game"); 
     JButton chngeSizeOnGame = new JButton("Byt storlek på spelplanen");
-    JButton changeColorOnNumbersButton = new JButton("Byt färg på brickorna");
-    JButton changeColorOnGameButton = new JButton("Byt färg på spelaplanen");
+    JButton changeColorOnNumbersButton = new JButton("Byt färg på spelbrickorna"); 
+    JButton changeColorOnGameButton = new JButton("Byt färg på spelaplanen"); 
     
     JButton exitButton = new JButton("Exit");
     northPanel(){
-        add(newGameButton);
+        add(newGameButton); 
         add(chngeSizeOnGame);
-        add(changeColorOnNumbersButton);
-        add(changeColorOnGameButton);
-        add(exitButton);
-        exitButton.addActionListener(this);
+        add(changeColorOnNumbersButton); 
+        add(changeColorOnGameButton); 
+        add(exitButton); 
+        exitButton.addActionListener(this); 
     }
 
     @Override
