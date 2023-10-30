b285085ece865558a65c5507fb42919adc151686
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2023-10-30 09:45:32 +0100

    Adderade changeColorOnGame/changeColorOnNumbers

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main extends JFrame implements ActionListener {
     public static void main(String[] args) {
@@ -55,13 +58,43 @@ public class Main extends JFrame implements ActionListener {
         southPanel.moveCounter++;
         southPanel.moveCounterLabel.setText("Antal drag: " + southPanel.moveCounter);
     }
+
+    public ArrayList<JButton> shuffleGame(ArrayList<JButton> buttonlist) {
+        Collections.shuffle(buttonlist);
+        return buttonlist;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (isMoveOk(buttonPosition(e.getActionCommand(), centerPanel.dimensionArray))){
+        if (isMoveOk(buttonPosition(e.getActionCommand(), centerPanel.dimensionArray))) {
             int[] clickedButtonPosition = convertStringToIntArray(buttonPosition(e.getActionCommand(), centerPanel.dimensionArray));
-            move1(clickedButtonPosition[0],clickedButtonPosition[1]);
+            move1(clickedButtonPosition[0], clickedButtonPosition[1]);
+        }
+        if (e.getSource() == northPanel.newGameButton) {
+            shuffleGame(centerPanel.buttonList);
+            
+        }
+        if (e.getSource() == northPanel.changeColorOnNumbersButton) {
+            Color colorSelectorNumbers = JColorChooser.showDialog(null, "Välj en färg på spelbrickorna", Color.WHITE);
+            if (colorSelectorNumbers != null) {
+                Component[] comps = centerPanel.getComponents();
+                for (Component comp : comps) {
+                    if (comp instanceof JButton b) {
+                        b.setBackground(colorSelectorNumbers);
+                    }
+                }
+            }
+        }
+        if (e.getSource() == northPanel.changeColorOnGameButton) {
+            Color colorSelectorGame = JColorChooser.showDialog(null, "Välj en färg på spelplanen", Color.BLACK);
+            if (colorSelectorGame != null) {
+                centerPanel.setBackground(colorSelectorGame);
+                northPanel.setBackground(colorSelectorGame);
+            }
         }
     }
+
+
     public String buttonPosition(String buttonNumber, JButton[][] dimensionArray) {
         String searchString = buttonNumber;
 
@@ -80,24 +113,26 @@ public class Main extends JFrame implements ActionListener {
                 break;
             }
         }
-        return rowPosition + ":"+ colPosition;
+        return rowPosition + ":" + colPosition;
     }
-    public boolean isMoveOk(String clickedButtonPosition){
+
+    public boolean isMoveOk(String clickedButtonPosition) {
         boolean returnboolean = false;
 
         int[] clickedbuttonPositionParts = convertStringToIntArray(clickedButtonPosition);
         int[] blankButtonPosition = convertStringToIntArray(buttonPosition(" ", centerPanel.dimensionArray));
         if (clickedbuttonPositionParts[0] == (blankButtonPosition[0]) &&
                 (blankButtonPosition[1] == clickedbuttonPositionParts[1] + 1 ||
-                blankButtonPosition[1] == clickedbuttonPositionParts[1] - 1 )) {
+                        blankButtonPosition[1] == clickedbuttonPositionParts[1] - 1)) {
             returnboolean = true;
         } else if (clickedbuttonPositionParts[1] == (blankButtonPosition[1]) &&
                 (blankButtonPosition[0] == clickedbuttonPositionParts[0] + 1 ||
-                blankButtonPosition[0] == clickedbuttonPositionParts[0] - 1 )) {
+                        blankButtonPosition[0] == clickedbuttonPositionParts[0] - 1)) {
             returnboolean = true;
         }
         return returnboolean;
     }
+
     public static int[] convertStringToIntArray(String input) {
         String[] parts = input.split(":");
         int[] intArray = new int[parts.length];
@@ -108,7 +143,8 @@ public class Main extends JFrame implements ActionListener {
 
         return intArray;
     }
-    public void move1(int a, int b){
+
+    public void move1(int a, int b) {
         int[] blankButtonPosition = convertStringToIntArray(buttonPosition(" ", centerPanel.dimensionArray));
         centerPanel.dimensionArray[blankButtonPosition[0]][blankButtonPosition[1]].setText(centerPanel.dimensionArray[a][b].getText());
         centerPanel.dimensionArray[a][b].setText(" ");
